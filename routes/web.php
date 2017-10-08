b294a52433753b0e1579f1b7a3d3453f58a1c470
Saaraketha-Apps <kanishkas@99x.lk>
2017-10-08 13:12:54 +0530

    edit route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function(){
 
 //Route::post('/shopify/webhook-tagger','OrderController@webhook_tagger');
 
-Route::post('https://saarai-test.myshopify.com/recurring-orders','OrderController@reorder');
+Route::post('/shopify/recurring-orders','OrderController@reorder');
 
 //Route::post('/shopify/reorder','OrderController@reorder');
 //Route::post('/shopify/reorder','ReorderController@reorder');
