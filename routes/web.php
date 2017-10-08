5249f8d4597516095653ff9ce46e78f8997c2208
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-09 01:00:12 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function(){
 	return view('welcome');
 });
 
-//Route::post('/saarai-test-new/recurring_orders-new','OrderController@reorder');
+Route::post('/saarai-test-new/recurring_orders-new','OrderController@reorder');
