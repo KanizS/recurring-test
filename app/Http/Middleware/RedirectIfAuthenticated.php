72cd5a109d6ff8bc7dc8656d1d005d8cf72c6cc2
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-09 14:07:34 +0530

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-          //  return redirect('/home');
+            return redirect('/home');
         }
 //file_put_contents("php://stderr", "12345\n");
        return $next($request);
