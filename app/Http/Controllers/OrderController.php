0c5f34fcfe9b929fc17fd92984e98ff1b94cf732
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-09 18:30:11 +0530

    initialize attributes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,12 +10,13 @@ file_put_contents("php://stderr", "####################\n");
    
 	//traverse through note attributes
 	$note_attribute_count = (int)count($request['note_attributes']);
+		$_subscribe_order_name ='';
+		$_subscribe_order_value=''; 
 	
 	for($r=0;$r<$note_attribute_count;$r++){
 	
 		$_name = $request['note_attributes'][$r]['name'];
-		$_subscribe_order_name ='';
-		$_subscribe_order_value=''; 
+
 		switch ($_name){
 				 case 'include_gift_wrapping':
 					$_include_gift_wrapping_name = $_name;
