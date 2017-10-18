f68955d1693735a2dcd1d007779e3b5e3716a689
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2017-10-18 17:32:03 +0530

    testing the json data

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,17 +8,15 @@ class OrderController extends Controller
 	
 public function reorder(Request $request){
 	
-file_put_contents("php://stderr", "inside\n");
-	  file_put_contents("php://stderr", "$request\n");
-	$val = $request['id'];
-      file_put_contents("php://stderr", "$val\n");
+	file_put_contents("php://stderr", "inside\n");
+	file_put_contents("php://stderr", "$request\n");
 
-	$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
-	$client = new Client();
-	$RequestResponse = $client->get($url);
-	$x= $RequestResponse->getBody();
-	file_put_contents("php://stderr", "$x\n");
-	file_put_contents("php://stderr", "done\n");	
+	//$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
+	//$client = new Client();
+	//$RequestResponse = $client->get($url);
+	//$x= $RequestResponse->getBody();
+	//file_put_contents("php://stderr", "$x\n");
+	//file_put_contents("php://stderr", "done\n");	
 }
 	
  private function set_reorder($order){
