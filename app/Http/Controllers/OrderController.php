2f255e87b8ae8102725271323d848d0f920505cc
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-18 17:06:05 +0530

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,7 +15,7 @@ file_put_contents("php://stderr", "inside\n");
 	$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
 	$client = new Client();
 	$RequestResponse = $client->get($url);
-	$x=$RequestResponse['order']['id']['value'];
+	$x=$RequestResponse['order']['value'];
 	file_put_contents("php://stderr", "$x\n");
 	file_put_contents("php://stderr", "done\n");	
 }
