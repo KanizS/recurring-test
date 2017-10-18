18b0286b78d58c22bd59ceac46593eb12a0b2651
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2017-10-18 18:57:45 +0530

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,7 +9,10 @@ class OrderController extends Controller
 public function reorder(Request $request){
 	
 	file_put_contents("php://stderr", "inside\n");
-	file_put_contents("php://stderr", "$request\n");
+	$json = '["apple","orange","banana","strawberry"]';
+	$ar = json_decode($json);
+	// access first element of $ar array
+	echo $ar[0]; // apple
 	
 
 	//$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
