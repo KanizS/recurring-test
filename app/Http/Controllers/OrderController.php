6f427b3408bc65d190b5da740ff63ef3f62939b5
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-18 16:58:59 +0530

    get id

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,14 +6,17 @@ use GuzzleHttp\Client;
 class OrderController extends Controller
 {   
 	
-public function reorder(String $request){
+public function reorder(Request $request){
 	
 file_put_contents("php://stderr", "inside\n");
-      
+	$val = $request['id'];
+      file_put_contents("php://stderr", "$val\n");
 
 	$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
 	$client = new Client();
 	$RequestResponse = $client->get($url);
+	$x=$RequestResponse['id'];
+		file_put_contents("php://stderr", "$x\n");
 	file_put_contents("php://stderr", "done\n");	
 }
 	
