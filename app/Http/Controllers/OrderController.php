81212783505eb64bf6c69a2f8e58c3f68292e336
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-19 08:08:49 +0530

    get method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,12 +11,13 @@ public function reorder($id){
 	file_put_contents("php://stderr", "$id\n");
 	
 
-	//$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/116273315870.json');
-	//$client = new Client();
-	//$RequestResponse = $client->get($url);
-	//$x= $RequestResponse->getBody();
-	//file_put_contents("php://stderr", "$x\n");
-	//file_put_contents("php://stderr", "done\n");	
+	$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/');
+	$url = $url."$id".".json";
+	$client = new Client();
+	$RequestResponse = $client->get($url);
+	$x= $RequestResponse->getBody();
+	file_put_contents("php://stderr", "$x\n");
+	file_put_contents("php://stderr", "done\n");	
 }
 	
  private function set_reorder($order){
