74492a3bf37875a06b723252bd083e4262de0960
Saaraketha-Apps <32549678+Saaraketha-Apps@users.noreply.github.com>
2017-10-19 08:12:39 +0530

    get note attributes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,14 +10,18 @@ public function reorder($id){
 	
 	file_put_contents("php://stderr", "$id\n");
 	
-
+//get order by id
 	$url =(string)('https://919dbb1d353c767687732dccb73b3b6c:fba6ef04320dec52cf543b6b266f2b9e@saaraketha-organics.myshopify.com/admin/orders/');
 	$url = $url."$id".".json";
 	$client = new Client();
 	$RequestResponse = $client->get($url);
-	$x= $RequestResponse->getBody();
-	file_put_contents("php://stderr", "$x\n");
+	$RequestResponse= $RequestResponse->getBody();
+	file_put_contents("php://stderr", "$RequestResponse\n");
 	file_put_contents("php://stderr", "done\n");	
+	
+//get order elements
+	$note_attribute_count = (int)count($RequestResponse['note_attributes']);
+	file_put_contents("php://stderr", "$note_attribute_count\n");	
 }
 	
  private function set_reorder($order){
